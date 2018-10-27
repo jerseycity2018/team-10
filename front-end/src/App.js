a8fd7ce1d5b4d6b6f54409866558af202fe57772
Daniel Constantine <danielkevinconstantine@gmail.com>
2018-10-27 05:05:45 -0400

    real time updated data

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -20,6 +20,9 @@ import { Graph } from "./component/Graph";
 import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
 
+import * as d3 from "d3";
+import { SSL_OP_MICROSOFT_BIG_SSLV3_BUFFER } from "constants";
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -27,6 +30,7 @@ class App extends Component {
       sliderA: 0,
       sliderB: 0,
       time: 0,
+      timeIndex: 0,
       dayArray: [
         "Sunday",
         "Monday",
@@ -36,28 +40,109 @@ class App extends Component {
         "Friday",
         "Saturday"
       ],
-      day: "Sunday"
+      day: "Sunday",
+      data: []
     };
 
     this.onChangeFirstSlider = this.onChangeFirstSlider.bind(this);
     this.onChangeSecondSlider = this.onChangeSecondSlider.bind(this);
+    this.findDay = this.findDay.bind(this);
+    this.findTime = this.findTime.bind(this);
   }
 
   onChangeFirstSlider(e) {
-    // console.log("the time " + e); //console.log the value from the scroll
-    this.setState({
-      time: e + ":00"
-    });
+    this.setState(
+      {
+        time: e + ":00"
+      },
+      this.findTime(e)
+    );
   }
 
   onChangeSecondSlider(e) {
     // console.log("the date " + e);
-    this.setState({
-      day: this.state.dayArray[e]
-    });
+    this.findDay(this.state.day);
+    this.setState(
+      {
+        day: this.state.dayArray[e]
+      }
+      // this.findDay(this.state.day)
+    );
+  }
+
+  findDay(e) {
+    let columns = new Array(24);
+    let path = "./data/";
+    let specificPath = "";
+
+    // switch (e) {
+    //   case "Sunday":
+    //     specificPath = "SUN";
+    //     // console.log("enter");
+    //     break;
+    //   case "Monday":
+    //     specificPath = "MON";
+    //     console.log("enter");
+    //     break;
+    //   case "Tuesday":
+    //     specificPath = "TUE";
+    //     break;
+    //   case "Wednesday":
+    //     specificPath = "WED";
+    //     break;
+    //   case "Thursday":
+    //     specificPath = "THU";
+    //     break;
+    //   case "Friday":
+    //     specificPath = "FRI";
+    //     break;
+    //   case "Saturday":
+    //     specificPath = "SAT";
+    //     break;
+    //   default:
+    // }
+    // path = path + specificPath;
+    // path =
+    //   path +
+    //   "/" +
+    //   specificPath.toLocaleLowerCase() +
+    //   this.state.timeIndex +
+    //   ".csv";
+    // console.log(path);
+    path = "./data/MON/mon0.json";
+    fetch(path)
+      .then(response => response.json())
+      .then(data => {
+        let abc = [];
+        for (let i = 0; i < data.length; i++) {
+          abc.push({
+            center: [data[i].lat, data[i].long],
+            radius: 0.015, //we can multiply this to show how populated the area
+            points: 100,
+            option: {
+              fillColor: "yellow",
+              strokeColor: "red",
+              strokeThickness: 0.1
+            }
+          });
+        }
+        this.setState({
+          data: abc
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    // console.log(this.state.data);
+  }
+
+  findTime(e) {
+    // console.log(e);
   }
 
   render() {
+    // console.log(this.state.data);
     return (
       <div className="container-fluid cover">
         <div className="row">
@@ -66,7 +151,7 @@ class App extends Component {
             <div className="container">
               <div className="row">
                 <div className="col">
-                  <Graph />
+                  <Graph data={this.state.data} />
                   <div className="row scrollbar">
                     <div className="col">
                       <Slider

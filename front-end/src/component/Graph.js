a81088cb29bdaf73a2afc9e5ec75318e1c150b00
Daniel Constantine <danielkevinconstantine@gmail.com>
2018-10-27 02:21:16 -0400

    adding new points works in map

--- front-end/src/component/Graph.js
+++ front-end/src/component/Graph.js
@@ -6,7 +6,7 @@ export class Graph extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      key: ""
+      key: "AoT3mpFuBYAL0kcQfWBFPlLH23U-6Y-uR_BSBuN9oJOaKPYsa-qolk0fRG-s5hsU"
     };
   }
 
@@ -23,6 +23,16 @@ export class Graph extends React.Component {
           regularPolygons={[
             {
               center: [40.7829, -73.9654],
+              radius: 0.02, //we can multiply this to show how populated the area
+              points: 100,
+              option: {
+                fillColor: "yellow",
+                strokeColor: "red",
+                strokeThickness: 0.1
+              }
+            },
+            {
+              center: [40.78, -73.9654],
               radius: 0.02, //we can multiply this to show how populated the area
               points: 100,
               option: {

fb6ee35fa34c7cb7f6b5256310e919b42cad54eb
Daniel Constantine <danielkevinconstantine@gmail.com>
2018-10-26 22:54:10 -0400

    Adding scrollbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -13,7 +13,50 @@ import "./App.css";
 import { LandingPage } from "./component/LandingPage";
 import { Footer } from "./Footer";
 
+import Slider from "rc-slider";
+// We can just import Slider or Range to reduce bundle size
+// import Slider from 'rc-slider/lib/Slider';
+// import Range from 'rc-slider/lib/Range';
+import "rc-slider/assets/index.css";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sliderA: 0,
+      sliderB: 0,
+      time: 0,
+      dayArray: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ],
+      day: "Sunday"
+    };
+
+    this.onChangeFirstSlider = this.onChangeFirstSlider.bind(this);
+    this.onChangeSecondSlider = this.onChangeSecondSlider.bind(this);
+  }
+
+  onChangeFirstSlider(e) {
+    // console.log("the time " + e); //console.log the value from the scroll
+    this.setState({
+      time: e + ":00"
+    });
+  }
+
+  onChangeSecondSlider(e) {
+    // console.log("the date " + e);
+    this.setState({
+      day: this.state.dayArray[e]
+    });
+  }
+
   render() {
     return (
       <div className="container-fluid landing">
@@ -24,7 +67,26 @@ class App extends Component {
                 <div className="col">
                   <LandingPage />
                   <div className="graph" />
-                  <Footer />
+                  <div className="row scrollbar">
+                    <div className="col">
+                      <Slider
+                        onChange={this.onChangeSecondSlider}
+                        min={0}
+                        max={6}
+                        defaultValue={0}
+                      />
+                      <p>Day: {this.state.day}</p>
+                    </div>
+                    <div className="col">
+                      <Slider
+                        onChange={this.onChangeFirstSlider}
+                        min={0}
+                        max={23}
+                        defaultValue={0}
+                      />
+                      <p>Time: {this.state.time}</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
--- front-end/src/component/LandingPage.js
+++ front-end/src/component/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink, Switch, Route } from "react-router-dom";
 
 export class LandingPage extends React.Component {
